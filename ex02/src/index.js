c0f0c922511c930d725a310be3004d9e3ae5d6f7
Kserhaja <kenanserhatlic@hotmail.com>
2021-03-25 23:45:59 +0100

    preparation

---
+++ ex02/src/index.js
@@ -0,0 +1,21 @@
+function* myGenerator() {
+    yield insideGenerator1;
+    yield insideGenerator2();
+    yield insideGenerator3();
+
+}
+
+function* insideGenerator1() {
+    
+
+}
+function* insideGenerator2() {
+
+}
+
+function* insideGenerator3() {
+
+}
+
+const iterator = insideGenerator1
+module.exports = { fifteenArray, myGenerator };
