7d4e8b744110b1ca112851375c5db39c85dc1103
Kserhaja <kenanserhatlic@hotmail.com>
2021-03-25 22:18:04 +0100

    push finnal

--- ex01/src/index.js
+++ ex01/src/index.js
@@ -1,26 +1,16 @@
 let myArr = [];
-// Only change code below this line
-
-function* multiplication(x){
-    for (let i = 0; i < 3; i++) {
-        x *= 2;
-        
-        yield x;
-
-    }
-       
+//Only change code below this line
+function* multiplication(x) {
+  for (i = x + 1; i >= 0; i--) {
+    x *= 2;
+    yield x;
+    myArr.push(x);
+  }
+  return x;
 }
 var num = multiplication(3);
-myArr.push(x);
-for (number of num){
-
-    console.log(num.next().value);
-    console.log(num.next().value); 
-    console.log(num.next().value); 
-    console.log(num.next().value);     
-}    
-
-    
+for (let i = 0; i < 3; i++) {
+  console.log(num.next().value);
+}
 // Only change code above this line
-
-module.exports = multiplication;
+module.exports = multiplication; 
