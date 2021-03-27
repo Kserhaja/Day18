174cc108429e76134e3a862303ba0806ff5ad492
Kserhaja <kenanserhatlic@hotmail.com>
2021-03-27 18:42:33 +0100

    ex03 done

--- ex03/src/index.js
+++ ex03/src/index.js
@@ -1,12 +1,39 @@
-function* myGenerator(arguments) {
-    arguments = 6, 7, 8;
+// change code below this line
+function* myGenerator() {
     yield* [1, 2, 3, 4, 5];
-    yield* "Arena";
+    yield* 'Arena';
     yield* arguments;
 }
-generatorArray();
-// Only change code bellow this line
+// change code above this line
+const iterator = myGenerator(6, 7, 8); // const iterator
+var generatorArray = [];
+for (let i = 0; i < 13; i++) {
+    generatorArray.push(iterator.next().value);
+}
+// for loop
+for (let i = 0; i < generatorArray.length; i++) {
+    if (i == 0) {
+        console.log("[ " + generatorArray[i]);
+    } else if ( i == generatorArray.length - 1) {
+        console.log(generatorArray[i] + " ]");
+    } else {
+        console.log(generatorArray[i] + ", ");
+    }
+}
+// Only change code below this line
+module.exports = { generatorArray, myGenerator}; // change this line
 
-// Only change code above this line
 
-module.exports = { generatorArray, myGenerator };
+
+
+
+
+
+
+
+
+
+
+
+
+
