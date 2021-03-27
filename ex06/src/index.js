5b7e3cfcbad69404fce996efc331b34f83770bb9
Kserhaja <kenanserhatlic@hotmail.com>
2021-03-27 18:31:29 +0100

    ex06 Mick Jagger

--- ex06/src/index.js
+++ ex06/src/index.js
@@ -1,4 +1,11 @@
-
+let rockStar = new Map([
+    ["artist", "The Rolling Stones"],
+    ["song", "Angie"],
+    ["album", "Goats Head Soup"],
+    ["singer", "Mick Jagger"],
+  ]);
+  let newRock = new Map([...rockStar].filter(([k, v]) => k[0] > "a"));
+  console.log(newRock);
 
 // Only change code bellow this line
 
