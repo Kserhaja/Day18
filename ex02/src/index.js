2d97f4f2b2784a50d25eeebca2e0ed58f591b63a
Kserhaja <kenanserhatlic@hotmail.com>
2021-03-27 17:11:33 +0100

    something

--- ex02/src/index.js
+++ ex02/src/index.js
@@ -1,26 +1,37 @@
 
 function* myGenerator() {
-    yield insideGenerator1;
+    yield insideGenerator1();
     yield insideGenerator2();
     yield insideGenerator3();
-
 }
-
 
 function* insideGenerator1() {
-    
-
+    for (let i = 1; i <= 5; i++) {
+        yield x = i;
+        fifteenArray.push(i+"#");
+    }
 }
 function* insideGenerator2() {
+    for (let i = 10; i <= 15; i++); {
+        yield x = i;
+        fifteenArray.push(i + "#");
+    }
 
 }
-
 function* insideGenerator3() {
-
+    for (let i = 6; i <= 9; i++); {
+        yield x = i;
+        fifteenArray.push(i + "#");
+    }
 }
 
-fifteenArray();
+
+
 const iterator = myGenerator();
-console.log(iterator.next());
+const fifteenArray = [];
+for (let i = 0; i < 16; i++) {
+    iterator.next().value;
+}
+console.log(fifteenArray);
 
 module.exports = { fifteenArray, myGenerator };
